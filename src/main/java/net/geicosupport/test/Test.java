b197e2241f540980bf3b8f646172ff5550f5bda1
geicosupport <epictrollface4200@gmail.com>
2025-06-29 02:11:28 -0500

    Initial

---
+++ src/main/java/net/geicosupport/test/Test.java
@@ -0,0 +1,16 @@
+package net.geicosupport.test;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test implements ModInitializer {
+	public static final String MOD_ID = "test";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/geicosupport/test/TestModClient.java
@@ -0,0 +1,10 @@
+package net.geicosupport.test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
